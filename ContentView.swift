086359ee11fc592c6583c1a29b0e6a49206d9db2
Lindokuhle Khumalo <klindokuhle1@gmail.com>
2025-08-16 18:43:50 +0200

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NetworkSwiftConcurrency
+//
+//  Created by Lindokuhle Khumalo on 2025/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkSwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkSwiftConcurrencyApp.swift
+//  NetworkSwiftConcurrency
+//
+//  Created by Lindokuhle Khumalo on 2025/08/16.
+//
+
+import SwiftUI
+
+@main
+struct NetworkSwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
